0c22eba6913c3380387e8508f459c14c59ea4925
vaishnavi483 <vaishnavisinha766@gmail.com>
2026-02-09 11:22:32 +0530

    Add files via upload

---
+++ firstprogramoflabmanual.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+      
+int main()
+      {
+      int x,y,z;
+      //printf("Welcome to BTech G2G\n");
+      printf("Enter the values of x and y");
+      scanf("%d%d",&x,&y);
+      z=x+y;
+      printf("Sum=%d",z);
+      return 0;
+      }
